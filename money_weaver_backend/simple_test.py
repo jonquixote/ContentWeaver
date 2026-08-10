510176f61eef879ee287aeca25696645dcc8ac61
John Hawley <jehawley@gmail.com>
2026-08-10 11:29:34 -0700

    chore: delete dead backend debug scripts and unused models/routes

--- money_weaver_backend/simple_test.py
+++
@@ -1,8 +0,0 @@
-print("Testing continuous narrative generation...")
-
-# Just import and test basic functionality
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
-print("Imports successful")
