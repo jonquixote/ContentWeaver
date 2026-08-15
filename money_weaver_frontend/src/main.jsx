93b5c81dd709077bdc88b971cc34edfda005e13e
John Hawley <jehawley@gmail.com>
2026-08-15 01:48:51 -0700

    feat: tanstack query, zustand, tiptap, vidstack; vite api proxy

--- money_weaver_frontend/src/main.jsx
+++ money_weaver_frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'next-themes'
+import { QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
+import { queryClient } from './lib/queryClient'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
